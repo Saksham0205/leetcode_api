3c7989635ff418f4a73aa97cf822132ea37b0f96
Saksham0205 <saksham252003@gmail.com>
2025-04-11 12:01:29 +0530

    first commit

--- index.py
+++ index.py
@@ -2,4 +2,9 @@ from api.handler import app
 from mangum import Mangum
 
 # Create handler for AWS Lambda / Vercel
-handler = Mangum(app)
+handler = Mangum(app, lifespan="off")
+
+# For local development
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
